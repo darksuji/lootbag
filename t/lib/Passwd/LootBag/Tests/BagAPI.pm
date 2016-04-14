7ce0d8a4ec3c839c8a366b972e8d67a4722f0c15
darksuji <darksuji@gmail.com>
2016-04-13 23:18:45 -0700

    Added failing one-item test

--- t/lib/Passwd/LootBag/Tests/BagAPI.pm
+++ t/lib/Passwd/LootBag/Tests/BagAPI.pm
@@ -23,15 +23,35 @@ class Passwd::LootBag::Tests::BagAPI extends Test::Class::Moose {
     method test_empty_bag($report) {
         $self->_make_empty_bag();
 
-        $self->tester
-            ->get_ok('/api/v0/bag/1')
-            ->status_is(HTTP_OK)
-            ->json_is( {} )
-        ;
+        my $t = $self->tester->get_ok('/api/v0/bag/1');
+
+        $t->status_is(HTTP_OK);
+        $t->json_is( {} );
     }
 
     method _make_empty_bag() {
 
+    }
+
+    method test_bag_with_one_item($report) {
+        $self->_make_bag_with_one_item();
+
+        my $t = $self->tester->get_ok('/api/v0/bag/1');
+
+        $t->status_is(HTTP_OK);
+        $t->json_is(
+            {
+                items => [
+                    '/api/v0/items/1'
+                ],
+            }
+        );
+    }
+
+    method _make_bag_with_one_item() {
+        my $bag = $self->_make_empty_bag();
+
+        return $bag;
     }
 }
 
